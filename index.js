c5151ef56fd1f3e536927fbb881619da5dea21a0
nandanaraju <nandanav0074@gmail.com>
2024-10-26 00:12:29 +0530

    Chaincode

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const ProductContract = require('./lib/product-contract');
+const OrderContract = require('./lib/order-contract');
+
+
+module.exports.ProductContract = ProductContract;
+module.exports.OrderContract = OrderContract;  
+
+module.exports.contracts = [ProductContract,OrderContract];
